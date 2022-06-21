9b9e70de9c70bff56ad6272d93de8edb243ff69e
Nacer <41012118@parisnanterre.fr>
2022-06-22 01:27:46 +0200

    first app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { tofsnap } from './models/tof-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snap-tof';
+export class AppComponent implements OnInit {
+
+  facesnapsTab!:tofsnap[];
+
+  ngOnInit(): void {
+
+
+
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
+import { TofSnapComponent } from './tof-snap/tof-snap.component';
+import { TofSnapListComponent } from './tof-snap-list/tof-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TofSnapComponent,
+    TofSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,useValue:'fr-FR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    registerLocaleData(fr.default);
+  }
+}
